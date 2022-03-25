24b147a58090084d82857bde217c90e7ab7a5a92
Lucas Denipoti <lucasfdenipoti@gmail.com>
2022-03-25 20:30:24 -0300

    Tarefa concluída
    pronto

---
+++ app/src/main/java/com/example/ohata_25_03_22/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ohata_25_03_22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+    }
+}
